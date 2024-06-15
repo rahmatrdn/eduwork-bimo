9568bf66d8524a41adf55e880abf3507718c74b6
FBimo <f.bimo31@gmail.com>
2024-06-15 16:36:36 +0700

    [Refactor]Use cookie

--- Final-Project-Task-Manager/middlewares/jwtMiddleware.go
+++ Final-Project-Task-Manager/middlewares/jwtMiddleware.go
@@ -1,15 +1,13 @@
 package middlewares
 
 import (
-	"fmt"
+	"eduwork-bimo/Final-Project-Task-Manager/helper"
 	"net/http"
-	"strings"
-	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-var jwtKey = []byte("secret_key")
+var jwtKey = []byte("ashdjqy9283409bsdklkg8hda02")
 
 type Claims struct {
 	UserID   int    `json:"user_id"`
@@ -19,25 +17,47 @@ type Claims struct {
 
 func JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("Authorization")
-		if tokenString == "" {
-			http.Error(w, "Missing token", http.StatusUnauthorized)
-			return
+
+		c, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				response := map[string]string{"message": "Unauthorized"}
+				helper.ResponseJSON(w, http.StatusUnauthorized, response)
+				return
+			}
 		}
-		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+		tokenString := c.Value
 
 		claims := &Claims{}
+		// parsing token jwt
 		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 			return jwtKey, nil
 		})
 
 		if err != nil {
-			http.Error(w, "Invalid token", http.StatusUnauthorized)
-			return
+			v, _ := err.(*jwt.ValidationError)
+			switch v.Errors {
+			case jwt.ValidationErrorSignatureInvalid:
+				// token invalid
+				response := map[string]string{"message": "Unauthorized invalid token"}
+				helper.ResponseJSON(w, http.StatusUnauthorized, response)
+				return
+			case jwt.ValidationErrorExpired:
+				// token expired
+				response := map[string]string{"message": "Unauthorized, Token expired!"}
+				helper.ResponseJSON(w, http.StatusUnauthorized, response)
+				return
+			default:
+				response := map[string]string{"message": "default Unauthorized"}
+				helper.ResponseJSON(w, http.StatusUnauthorized, response)
+				return
+			}
 		}
 
 		if !token.Valid {
-			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			response := map[string]string{"message": "Unauthorized"}
+			helper.ResponseJSON(w, http.StatusUnauthorized, response)
 			return
 		}
 
@@ -46,8 +66,10 @@ func JWTMiddleware(next http.Handler) http.Handler {
 }
 
 func GetUserIDFromToken(r *http.Request) (int, error) {
-	tokenString := r.Header.Get("Authorization")
-	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+	c, err := r.Cookie("token")
+
+	tokenString := c.Value
 
 	claims := &Claims{}
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
@@ -59,13 +81,7 @@ func GetUserIDFromToken(r *http.Request) (int, error) {
 	}
 
 	if !token.Valid {
-		fmt.Println("token invalid")
 		return 0, err
-	}
-
-	// Check if token is expired
-	if claims.ExpiresAt < time.Now().Unix() {
-		return 0, fmt.Errorf("token has expired")
 	}
 
 	// Return the user ID stored in the token
